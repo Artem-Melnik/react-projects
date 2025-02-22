7787219a91cbeaf9040c16d1fd0b09f829229b06
DesignHacker <66458748+Artem-Melnik@users.noreply.github.com>
2025-02-22 11:41:14 -0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const tourData = [
+    {
+      id: 1,
+      title: 'Mountain',
+      img: 'https://images.unsplash.com/photo-1454496522488-7a8e488e8606?q=80&w=2676&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 2,
+      title: 'City',
+      img: 'https://images.unsplash.com/photo-1444723121867-7a241cacace9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+    {
+      id: 3,
+      title: 'Forest',
+      img: 'https://images.unsplash.com/photo-1473448912268-2022ce9509d8?q=80&w=1441&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    },
+  ];
+
+  return (
+    <>
+      <div>
+        {tourData.map((tour) => (
+          <Card src={tour.img} title={tour.title} num={tour.id} />
+        ))}
+      </div>
+      {/* <Card />
+      <Card />
+      <Card /> */}
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+interface CardProps {
+  src: string;
+  title: string;
+  num: number;
+}
+
+const Card = (props: CardProps) => {
+  return (
+    <div style={{ border: 'solid 2px', borderRadius: '10px', margin: '10px' }}>
+      <h1>
+        {props.num}: {props.title}
+      </h1>
+      <img style={{ width: '500px' }} src={props.src}></img>
+    </div>
+  );
+};
+
+export default App;
